8c8cb8e79cdc38b4b364d9cdb2be82328d372546
Keeler Russell <keeler.russell@gmail.com>
2023-09-02 11:08:24 -0500

    feat: Initial table

---
+++ index.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", (event) => main());
+
+const WEEKDAYS = "MTWRF".split("");
+const MIN_HOUR = 6; // 6am
+const MAX_HOUR = 17;  // 5pm
+const MINUTES_PER_ROW = 5;
+
+const main = () => {
+    const rootElement = document.getElementById("root")
+    createCalendarTable(rootElement);
+}
+
+const data = () => {
+  return [
+    {
+      department: "BIOL",
+      classNumber: 102,
+      startTime: "0800",
+      endTime: "0915",
+      days: "MWR",
+      building: "Hubbard",
+      roomNumber: 301,
+    },
+    {
+      department: "BIOL",
+      classNumber: 106,
+      startTime: "0900",
+      endTime: "1015",
+      days: "MR",
+      building: "Hubbard",
+      roomNumber: 401,
+    },
+    {
+      department: "CHEM",
+      classNumber: 206,
+      startTime: "0900",
+      endTime: "0945",
+      days: "MWR",
+      building: "McKinley",
+      roomNumber: 301,
+    }
+  ]
+}
+
+const createCalendarTable = (parentElement) => {
+  const table = document.createElement("table");
+  parentElement.appendChild(table);
+
+  const headerNames = ["Time", ...WEEKDAYS];
+  // Set up the table header.
+  const header = document.createElement("tr");
+  table.appendChild(header);
+  headerNames.forEach((weekday) => {
+    const colHeader = document.createElement("th");
+    colHeader.textContent = weekday;
+    header.appendChild(colHeader);
+  });
+
+  // Set up the table contents.
+  const tableBody = document.createElement("tbody");
+  table.appendChild(tableBody);
+  const numRows = (MAX_HOUR - MIN_HOUR) * 60 / MINUTES_PER_ROW;
+  [...Array(numRows).keys()].forEach((rowNumber) => {
+    const tableRow = document.createElement("tr");
+    tableBody.appendChild(tableRow);
+    headerNames.forEach((header, index) => {
+      const rowCell = document.createElement("td");
+      if (index === 0) {
+        rowCell.textContent = rowNumber * 5;
+      } else {
+        rowCell.textContent = header;
+      }
+      tableRow.appendChild(rowCell);
+    })
+  });
+};
