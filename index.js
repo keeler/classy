4541adf2f16a2a3fbac8913b9d2d41c3aabe13bf
Keeler Russell <keeler.russell@gmail.com>
2023-09-02 11:44:25 -0500

    feat: Lines for hours and col labels

--- index.js
+++ index.js
@@ -21,19 +21,65 @@ const main = () => {
 }
 
 const createCalendarGrid = (svgElement) => {
+  // Overall border rect
   const yOffset = 10;
-  const outsideBorder = createRect(TIME_LABEL_OFFSET, yOffset, TOTAL_WIDTH, TOTAL_HEIGHT);
+  const outsideBorder = rect(TIME_LABEL_OFFSET, yOffset, TOTAL_WIDTH, TOTAL_HEIGHT);
   svgElement.appendChild(outsideBorder);
 
+  // Column rects + labels
   WEEKDAYS.forEach((weekday, index) => {
     const colX = TIME_LABEL_OFFSET + COLUMN_WIDTH * index;
-    const columnRect = createRect(colX, yOffset, COLUMN_WIDTH, TOTAL_HEIGHT);
+    const columnRect = rect(colX, yOffset, COLUMN_WIDTH, TOTAL_HEIGHT);
     svgElement.appendChild(columnRect);
+
+    const labelX = colX + COLUMN_WIDTH / 2;
+    const labelText = weekday;
+    const colLabel = text(labelX, yOffset + HOUR_HEIGHT/2, labelText);
+    svgElement.appendChild(colLabel);
+  });
+
+  // Time lines + labels
+  const numHours = (MAX_HOUR - MIN_HOUR);
+  [...Array(numHours).keys()].forEach((i) => {
+    const lineY = (i + 1) * HOUR_HEIGHT;
+    const line = horizontalLine(TIME_LABEL_OFFSET, TIME_LABEL_OFFSET + TOTAL_WIDTH, lineY);
+    svgElement.appendChild(line);
+
+    const labelName = (MIN_HOUR + i).toString();
+    const label = text(0, lineY, labelName);
+    svgElement.appendChild(label);
   });
 };
 
-const createRect = (x, y, w, h) => {
-  return getElement("rect", {"fill": "none", stroke: "black", "x": x, "y": y, "width": w, "height": h});
+const text = (x, y, content) => {
+  const textElem = getElement("text", {
+    x: x,
+    y: y
+  });
+  textElem.textContent = content;
+  return textElem;
+}
+
+const horizontalLine = (x1, x2, y) => {
+  return getElement("line", {
+    stroke: "grey",
+    "stroke-dasharray": "5,5",
+    x1: x1,
+    x2: x2,
+    y1: y,
+    y2: y
+  });
+}
+
+const rect = (x, y, w, h) => {
+  return getElement("rect", {
+    fill: "none",
+    stroke: "black",
+    x: x,
+    y: y,
+    width: w,
+    height: h
+  });
 };
 
 const getElement = (name, properties) => {
