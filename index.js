05af4e10bd5516de01840c0d11a0ccec710ed5d5
Keeler Russell <keeler.russell@gmail.com>
2023-09-02 15:54:42 -0500

    feat: Upload CSV

--- index.js
+++ index.js
@@ -6,40 +6,8 @@ const MAX_HOUR = 17;  // 5pm
 const MINUTES_PER_ROW = 5;
 
 const main = () => {
-    const rootElement = document.getElementById("root")
-    createCalendarTable(rootElement);
-}
-
-const data = () => {
-  return [
-    {
-      department: "BIOL",
-      classNumber: 102,
-      startTime: "0800",
-      endTime: "0915",
-      days: "MWR",
-      building: "Hubbard",
-      roomNumber: 301,
-    },
-    {
-      department: "BIOL",
-      classNumber: 106,
-      startTime: "0900",
-      endTime: "1015",
-      days: "MR",
-      building: "Hubbard",
-      roomNumber: 401,
-    },
-    {
-      department: "CHEM",
-      classNumber: 206,
-      startTime: "0900",
-      endTime: "0945",
-      days: "MWR",
-      building: "McKinley",
-      roomNumber: 301,
-    }
-  ]
+  const rootElement = document.getElementById("root")
+  createCalendarTable(rootElement);
 }
 
 const createCalendarTable = (parentElement) => {
@@ -74,3 +42,58 @@ const createCalendarTable = (parentElement) => {
     })
   });
 };
+
+const getScheduleData = () => {
+  return [
+    {
+      department: "BIOL",
+      classNumber: 102,
+      startTime: "0800",
+      endTime: "0915",
+      days: "MWR",
+      building: "Hubbard",
+      roomNumber: 301,
+    },
+    {
+      department: "BIOL",
+      classNumber: 106,
+      startTime: "0900",
+      endTime: "1015",
+      days: "MR",
+      building: "Hubbard",
+      roomNumber: 401,
+    },
+    {
+      department: "CHEM",
+      classNumber: 206,
+      startTime: "0900",
+      endTime: "0945",
+      days: "MWR",
+      building: "McKinley",
+      roomNumber: 301,
+    }
+  ]
+}
+
+function readSingleFile(evt) {
+  var f = evt.target.files[0];
+  if (f) {
+    var r = new FileReader();
+    r.onload = function(e) {
+        var contents = e.target.result;
+        document.write("File Uploaded! <br />" + "name: " + f.name + "<br />" + "content: " + contents + "<br />" + "type: " + f.type + "<br />" + "size: " + f.size + " bytes <br />");
+
+        var lines = contents.split("\n"), output = [];
+        for (var i=0; i<lines.length; i++){
+          output.push("<tr><td>" + lines[i].split(",").join("</td><td>") + "</td></tr>");
+        }
+        output = "<table>" + output.join("") + "</table>";
+        document.write(output);
+   }
+    r.readAsText(f);
+    document.write(output);
+  } else {
+    alert("Failed to load file");
+  }
+}
+document.getElementById('fileinput').addEventListener('change', readSingleFile);
